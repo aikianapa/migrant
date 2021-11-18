afd83497459cd1d31f39d9f92dcfcd073130ba78
Oleg Frolov <oleg_frolov@mail.ru>
2021-11-18 23:29:20 +0300

    update

--- modules/printdocx/printdocx.php
+++ modules/printdocx/printdocx.php
@@ -19,11 +19,6 @@ class modPrintdocx
         $app = $this->app;
         $data = $app->itemToArray($app->vars('_post'));
         $tid = str_replace('__','_',date('dmY',strtotime($data['_created'])).'_'.$data['doc_ser'].'_'.$data['doc_num']);
-        $path = $app->route->path_app.'/ocr/';
-        $file = $path.'approve.docx';
-        $tmp = $path.$tid.'.docx';
-        $pdf = $path.$tid.'.pdf';
-
         foreach ($data as $k => $v) {
             strpos(' '.$k, 'date') ? $data[$k] = date('d.m.Y', strtotime($v)) : null;
             strpos(' '.$k, 'expire') ? $data[$k] = date('d.m.Y', strtotime($v)) : null;
@@ -38,29 +33,36 @@ class modPrintdocx
         $data['reg_flat'] > ' ' ? $data['reg_flat'] = ', кв.'.$data['reg_flat'] : null;
         $data['doc_ser'] > ' ' ? $data['doc_ser'] = 'Серия '.$data['doc_ser'] : null;
 
-
         $ccodes = $app->treeRead('countries');
         $country = wbTreeFindBranch($ccodes['tree']['data'], $data['citizen']);
         isset($country[0]['name']) ? $data['citizen'] = $country[0]['name'] : null;
 
-        $phpWord = new \PhpOffice\PhpWord\TemplateProcessor($file);
-        foreach ($data as $fld => $val) {
-            $phpWord->setValue('{{'.$fld.'}}', $val);
+        $pdfsrc = '';
+        $pathsrc = $app->route->path_app.'/ocr/';
+        $pathtmp = $app->route->path_app.'/uploads/tmp/';
+
+        $list = ['approve.docx','persdata.docx'];
+        // 1) Читаем DOCX файлы 2) подставляем данные 3) сохраняем 4) экспортируем в PDF
+        foreach ($list as $key => $file) {
+            $file = $pathsrc.$file;
+            $tmp = $pathtmp.$tid.$key.'.docx';
+            $pdf = $pathtmp.$tid.$key.'.pdf';
+            $pdfsrc .= ' '.$tid.$key.'.pdf';
+            /*1*/   $phpWord = new \PhpOffice\PhpWord\TemplateProcessor($file);
+            /*2*/   $phpWord->setValues($data);
+            /*3*/   $phpWord->saveAs($tmp);
+                    chmod($tmp, 0777);
+            /*4*/   exec('export HOME='.$pathtmp.' && lowriter  --headless  --convert-to pdf --outdir '.$pathtmp.' '.$tmp);
+                    unlink($tmp);
         }
-        $phpWord->saveAs($tmp);
-        chmod($tmp, 0777);
-
-        exec('export HOME='.$path.' && lowriter  --headless  --convert-to pdf --outdir '.$path.' '.$tmp);
-        unlink($tmp);
-
-
+        // 5) Объединяем в один PDF
+        $pdf = $pathtmp.$tid.'.pdf';
+        exec('cd '.$pathtmp.' && rm -f '.$dstpdf.' && pdfunite '.$pdfsrc.' '.$pdf.' && rm '.$pdfsrc);
 
   $data = file_get_contents($pdf);
   header('Content-type: application/pdf');
   header('Content-Disposition: inline; filename="'.$tid.'.pdf"');
   unlink($pdf);
-
-
   echo $data;
 
 //        header('Content-Disposition:attachment;filename="downloaded.pdf"');
