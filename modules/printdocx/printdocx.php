96a6873f406f8528372c01c2ce73c254ee16001c
Oleg Frolov <oleg_frolov@mail.ru>
2022-02-07 12:31:13 +0300

    update

--- modules/printdocx/printdocx.php
+++ modules/printdocx/printdocx.php
@@ -29,13 +29,13 @@ class modPrintdocx
         $data->set('date', date('d.m.Y'));
         $data->set('reg_city', ucfirst($data['reg_city']));
         $data->set('reg_street', ucfirst($data['reg_street']));
-        $data->get('reg_corpse') > ' ' ? $item['reg_corpse'] = ', '.$item['reg_corpse'] : null;
+        $data->get('reg_corpse') > ' ' ? $item['reg_corpse'] = ', корп.'.$item['reg_corpse'] : null;
         $data->get('reg_flat') > ' ' ? $item['reg_flat'] = ', '.$item['reg_flat'] : null;
         $data->get('doc_ser') > ' ' ? $item['doc_ser'] = 'Серия '.$item['doc_ser'] : null;
 
 
             $data->get('region') > '' ? $data->set('reg_city_type', $data->get('region').' область, '.$data->get('reg_city_type')) : null; // Область + тип города
-            $data->get('reg_build') > '' ? $data->set('reg_corpse', $data->get('corpse').', стр. '.$data->get('reg_build')) : null; // Корпус + строение
+            $data->get('reg_build') > '' ? $data->set('reg_corpse', $data->get('reg_corpse').', стр. '.$data->get('reg_build')) : null; // Корпус + строение
             $data->set('reg_house', trim($data->get('reg_house').' '.$data->get('reg_house_num'))); // тип дома + номер дома
             $data->set('reg_flat', trim($data->get('reg_flat').' '.$data->get('reg_flat_num'))); // тип квартиры + номер квартиры
 
