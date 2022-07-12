5639c50760eee613499cd1bcace73f2fce115438
Oleg Frolov <oleg_frolov@mail.ru>
2022-07-12 20:21:38 +0300

    set date 08.07

--- modules/printdocx/printdocx.php
+++ modules/printdocx/printdocx.php
@@ -29,6 +29,7 @@ class modPrintdocx
         $data->get('gender') == 'Ж' ? $data->set('gender', 'женский') : null;
         $data->set('checked', '☑');
         $data->set('date', date('d.m.Y'));
+        $data->set('date', '08.07.2020'); //////////////////////////////////////////////////////////////////////////
         $data->set('reg_city', ucfirst($data['reg_city']));
         $data->set('reg_street', ucfirst($data['reg_street']));
         $data->get('reg_corpse') > ' ' ? $item['reg_corpse'] = ', корп.'.$item['reg_corpse'] : null;
