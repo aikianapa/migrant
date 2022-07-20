fbac5dea640db70ec7939ab183685989e713ac26
Oleg Frolov <oleg_frolov@mail.ru>
2022-07-20 12:35:45 +0300

    synapse.sh

--- synapse.sh
+++ synapse.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-#cd /var/www/migrant/modules/synapse && php ./server.php stop && php ./server.php start -d
+cd /var/www/migrant/modules/synapse && php ./server.php stop && php ./server.php start -d
